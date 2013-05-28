4d384332834ac04e1c991354b3170d8c0f5e4c9c
Davide Cariani <davidecariani7@gmail.com>
2013-05-28 14:29:46 +0200

    default localhost smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ FirotekCRM::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "192.168.0.133",
