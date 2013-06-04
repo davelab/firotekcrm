08c7fd835f390d7a1309ad41808d4b5e95b0fa3c
Davide Cariani <davidecariani7@gmail.com>
2013-06-04 18:41:48 +0200

    chenge assets debug settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ FirotekCRM::Application.configure do
   config.assets.compress = true
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
