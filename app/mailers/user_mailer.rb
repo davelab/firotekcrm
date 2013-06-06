eec777d043f054ea91be8cea235ec668f28e355e
Davide Cariani <davidecariani7@gmail.com>
2013-06-06 13:22:11 +0200

    bug fix task mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: "info@firotek.it"
 
   def new_task_assigned(user_email, owner, description)
+    @owner = owner
+    @description = description
       mail(:to => user_email, :subject => "[FirotekCRM] Nuovo task assegnato")
   end
 end
